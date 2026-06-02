d302df54530f358451ca93036c4ea1fe571b442c
satya1817 <thekhushi2008@gmail.com>
2026-06-02 13:30:36 +0530

    Implement user registration

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-
+const authRoutes = require("./routes/authRoutes");
 dotenv.config();
 
 connectDB();
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Expense Tracker API Running");
 });
+
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
