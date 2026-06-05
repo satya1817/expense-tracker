747813a77ef439f7310731e9ed312bdc2ae6efe1
satya1817 <thekhushi2008@gmail.com>
2026-06-05 10:13:05 +0530

    Add login and dashboard summary

--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -1,5 +1,53 @@
+import { useEffect, useState } from "react";
+import API from "../services/api";
+
 function Dashboard() {
-  return <h1>Dashboard Page</h1>;
+  const [summary, setSummary] = useState(null);
+
+  useEffect(() => {
+    const fetchSummary = async () => {
+      try {
+        const token = localStorage.getItem("token");
+
+        const res = await API.get(
+          "/transactions/summary",
+          {
+            headers: {
+              Authorization: token,
+            },
+          }
+        );
+
+        setSummary(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchSummary();
+  }, []);
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+
+      {summary && (
+        <>
+          <h3>
+            Total Income: ₹{summary.totalIncome}
+          </h3>
+
+          <h3>
+            Total Expense: ₹{summary.totalExpense}
+          </h3>
+
+          <h3>
+            Balance: ₹{summary.balance}
+          </h3>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default Dashboard;
--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,5 +1,73 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import API from "../services/api";
+
 function Login() {
-  return <h1>Login Page</h1>;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await API.post(
+        "/auth/login",
+        formData
+      );
+
+      localStorage.setItem(
+        "token",
+        res.data.token
+      );
+
+      alert("Login Successful");
+
+      navigate("/dashboard");
+    } catch (error) {
+      alert(error.response.data.message);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={handleChange}
+        />
+
+        <br /><br />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={handleChange}
+        />
+
+        <br /><br />
+
+        <button type="submit">
+          Login
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
