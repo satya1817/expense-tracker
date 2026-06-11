ed0976f775e486d3b5cf21ebb68e770d9d5c6467
satya1817 <thekhushi2008@gmail.com>
2026-06-11 11:10:33 +0530

    Connect frontend to Render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://expense-tracker-backend-mul7.onrender.com",
 });
 
 export default API;
