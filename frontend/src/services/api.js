e0bc3e7212f1f395ffc6f853e5530fbedd0b8587
satya1817 <thekhushi2008@gmail.com>
2026-06-11 11:32:05 +0530

    api error

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://expense-tracker-backend-mul7.onrender.com",
+  baseURL: "https://expense-tracker-backend-mul7.onrender.com/api",
 });
 
 export default API;
